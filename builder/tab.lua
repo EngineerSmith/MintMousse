a58c840cf35d2f0f014af94dd8e7ca59945d4c7f
EngineerSmith <pasmith.bus@gmail.com>
2023-07-27 18:38:07 +0100

    Added tab.addChildRaw

--- builder/tab.lua
+++ builder/tab.lua
@@ -16,8 +16,21 @@ tab.addComponent = function(self, ...)
   table.insert(self.components, component)
   return component
 end
+
 tab.addChild = tab.addComponent
+
+tab.addChildRaw = function(self, component, ...)
+  if component then
+    table.insert(self.components, component)
+    self:addChildRaw(...)
+  end
+  return self
+end
+
 tab.addSibling = function()
+  errorMintMousse("Builder tab: cannot add sibling to tab, as it is a root node.")
+end
+tab.addSiblingRaw = function()
   errorMintMousse("Builder tab: cannot add sibling to tab, as it is a root node.")
 end
 
