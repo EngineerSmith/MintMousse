48de64f2b9e2f29b778aaa28772c7023fe990a6d
EngineerSmith <pasmith.bus@gmail.com>
2023-07-27 21:10:42 +0100

    Started work on adding spinner component

--- global.lua
+++ global.lua
@@ -1,4 +1,4 @@
-return function(PATH, prefix)
+return function(PATH, prefix, log, warning, _error)
   requireMintMousse = function(file)
     return require(PATH .. file)
   end
@@ -6,37 +6,43 @@ return function(PATH, prefix)
   prefix = prefix or ""
   prefix = "Mintmousse " .. prefix .. ": "
 
-  local logging_log = prefix .. "log:"
-  logMintMousse = function(...)
-    print(logging_log .. table.concat({...}, " "))
-  end
-
-  local logging_warning = prefix .. "warning:"
-  warningMintMousse = function(...)
-    print(logging_warning .. table.concat({...}, " "))
-  end
-
-  local logging_error = prefix .. "error:"
-  errorMintMousse = function(...)
-    local str = table.concat({...}, " ")
-
-    if debug then
-      local info = debug.getinfo(2, "fnS")
-      if info then
-        local name
-        if info.name then
-          name = info.name
-        elseif info.func then -- Attempt to create a name from memory address
-          name = tostring(info.func):sub(10)
-        end
-        if info.short_src then
-          name = name .. "@" .. info.short_src .. (info.linedefined and "#" .. info.linedefined or "")
-        end
-        str = name .. ": " .. str
-      end
+  if log ~= false then -- note, nil is true
+    local logging_log = prefix .. "log:"
+    logMintMousse = function(...)
+      print(logging_log .. table.concat({...}, " "))
     end
+  end
 
-    print(logging_error .. str)
-    error(logging_error .. str)
+  if warning ~= false then
+    local logging_warning = prefix .. "warning:"
+    warningMintMousse = function(...)
+      print(logging_warning .. table.concat({...}, " "))
+    end
+  end
+
+  if _error ~= false then
+    local logging_error = prefix .. "error:"
+    errorMintMousse = function(...)
+      local str = table.concat({...}, " ")
+
+      if debug then
+        local info = debug.getinfo(2, "fnS")
+        if info then
+          local name = ""
+          if info.name then
+            name = info.name
+          elseif info.func then -- Attempt to create a name from memory address
+            name = tostring(info.func):sub(10)
+          end
+          if info.short_src then
+            name = name .. "@" .. info.short_src .. (info.linedefined and "#" .. info.linedefined or "")
+          end
+          str = name .. ": " .. str
+        end
+      end
+
+      print(logging_error .. str)
+      error(logging_error .. str)
+    end
   end
 end
