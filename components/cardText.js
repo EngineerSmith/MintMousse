9d7e27cefa949762c10b8874734bc5cc8884f8a8
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2025-04-03 17:25:44 +0100

    Added cardText component

---
+++ components/cardText.js
@@ -0,0 +1,25 @@
+function cardText_new(payload) {
+  const id = payload.id;
+  const text = payload.text;
+
+  const container = document.createElement("div");
+  container.classList.add("card-body");
+  container.setAttribute("id", id + "-root");
+
+  const p = document.createElement("p");
+  p.classList.add("card-text");
+  p.setAttribute("id", id);
+  p.textContent = text;
+
+  container.append(p);
+
+  return container;
+}
+
+function cardText_update_text(payload) {
+  const id = payload.id;
+  const text = payload.text;
+
+  const p = document.getElementById(id);
+  p.textContent = text;
+}
