1659f0033f6d33479dcf7b418e623e5e4dab65dc
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2025-09-09 20:09:46 +0100

    Added new component List

---
+++ components/list.js
@@ -0,0 +1,50 @@
+function list_new(payload) {
+  const id = payload.id;
+  const isNumbered = Boolean(payload.numbered ?? false);
+
+  const listGroup = document.createElement("ul");
+  listGroup.classList.add("list-group");
+  listGroup.setAttribute("id", id)
+
+  if (isNumbered === true) {
+    listGroup.classList.add("list-group-numbered");
+  }
+
+  return listGroup;
+}
+
+function list_update_numbered(payload) {
+  const id = payload.id;
+  const isNumbered = Boolean(payload.numbered ?? false);
+
+  const listGroup = document.getElementById(id);
+  if (isNumbered === true) {
+    listGroup.classList.add("list-group-numbered");
+  } else {
+    listGroup.classList.remove("list-group-numbered");
+  }
+}
+
+function list_insert(payload) {
+  const id = payload.parentID;
+  const childID = id + "-" + payload.id;
+
+  const listItem = document.createElement("li");
+  listItem.classList.add("list-group-item", "d-flex");
+  listItem.setAttribute("id", childID);
+
+  insertPayload(listItem, payload);
+
+  const listGroup = document.getElementById(id);
+  listGroup.append(listItem);
+
+  eventInit();
+}
+
+function list_remove_child(payload) {
+  const id = payload.parentID;
+  const childID = id + "-" + payload.id;
+
+  const listItem = document.getElementById(childID);
+  removeElement(listItem);
+}
