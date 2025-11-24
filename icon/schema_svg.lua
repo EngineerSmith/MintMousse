c50e9d61ef72edbadf12df6a9fb27c230aa54e34
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2025-11-24 22:57:40 +0000

    Refactored SVG favicon generation

---
+++ icon/schema_svg.lua
@@ -0,0 +1,45 @@
+local PATH = (...):match("^(.*)[^%.]+$")
+
+local webColors = require(PATH .. "webColors")
+
+local function validateColor(c, default)
+  if type(c) ~= "string" then
+    return default
+  end
+  -- If it's a known web color name, return it
+  if webColors[c] then
+    return c
+  end
+  -- If it's a hex color(#FFF or #FFFFFF), return it
+  if c:sub(1, 1) == "#" then
+    return c
+  end
+  return default
+end
+
+return function(icon)
+  icon = type(icon) == "table" and icon or { }
+
+  local viewModel = { }
+  if icon.shape == "circle" then
+    viewModel.borderRadius = 45
+  elseif icon.shape == "rectangle" or icon.shape == "square" then
+    viewModel.borderRadius = 0
+  else -- default squircle
+    viewModel.borderRadius = 20
+  end
+
+  viewModel.fillColor = validateColor(icon.insideColor or icon.color, "#95d7ab")
+  viewModel.strokeColor = validateColor(icon.outsideColor, "#4A7C59") -- #00FF70
+
+  if viewModel.strokeColor ~= "none" then
+    viewModel.strokeWidth = tonumber(icon.strokeWidth) or 4
+  else
+    viewModel.strokeWidth = 0
+  end
+
+  viewModel.emoji = type(icon.emoji) == "string" and icon.emoji or "🍮"
+  viewModel.easterEgg = icon.easterEgg == "MM"
+
+  return viewModel
+end
