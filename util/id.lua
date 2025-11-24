c65d48ab8d9e113b8b5c3a8afff3506ad83292d4
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2025-11-24 23:01:00 +0000

    Added util/id which contains the refactored ID management logic

---
+++ util/id.lua
@@ -0,0 +1,63 @@
+local PATH = (...):match("^(.-)[^%.]+$")
+
+local mintmousse = require(PATH .. "conf")
+
+local id = { }
+
+local base62_Alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+local base62Encode = function(num)
+  if num == 0 then
+    return base62_Alphabet:sub(1, 1)
+  end
+
+  local encoded, current = "", num
+  while current > 0 do
+    local remainder = current % 62
+    encoded = base62_Alphabet:sub(remainder + 1, remainder + 1) .. encoded
+    current = math.floor(current / 62)
+  end
+  return encoded
+end
+
+local idPrefix = "MM" .. mintmousse._threadID .. "_"
+local idCounter = 0
+id.generateID = function()
+  local newID = idPrefix .. base62Encode(idCounter)
+  idCounter = idCounter + 1
+  return newID
+end
+
+local protectedKeywords = {
+  ["all"]     = true,
+  ["unknown"] = true,
+}
+local invalidPattern = "[^w%._,:;@]"
+
+id.isValidID = function(str)
+  if type(str) ~= "string" then
+    return false, "ID isn't type string"
+  end
+
+  if #str == 0 then
+    return false, "ID cannot be empty"
+  end
+
+  local firstByte = str:byte(1)
+  if firstByte >= 48 and firstByte <= 57 then
+    return false, "ID cannot start with number"
+  end
+
+  if protectedKeywords[str] then
+    return false, "ID cannot use the protected keyword '" .. str .. "'"
+  end
+
+  local badChar = str:match(invalidPattern)
+  if badChar then
+    return false, "ID contains invalid character: '" .. badChar .. "'"
+  end
+
+  return true, nil
+end
+
+return id
--- util/init.lua
+++ util/init.lua
@@ -1,7 +1,19 @@
 local PATH = ...
-PATH = PATH:match("^(.*)%.init$") or PATH
+PATH = PATH:match("^(.*)%.[^%.]+$") or PATH
 local ROOT = PATH:match("^(.-)[^%.]+$") or ""
 PATH = PATH .. "."
 
+local util = { }
+
 local cleanupTraceback = require(ROOT .. "logging.cleanupTraceback")
-love.mintmousse._cleanupTraceback = cleanupTraceback
+util.cleanupTraceback = cleanupTraceback
+
+-- Only use this if necessary. All MintMousse components will handle sanitizing for you.
+--   If you have non-standard components, it may help to sanitize to avoid XSS attacks
+--   unless you know you've programmed them correctly.
+util.sanitizeText = function(text)
+  local lustache = love.mintmousse.require("libs.lustache")
+  return lustache:render("{{text}}", { text = text })
+end
+
+return util
