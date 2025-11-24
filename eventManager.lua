f08bba9d4e6c4f770af2b34d011ce4850859c022
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2025-11-24 23:04:19 +0000

    eventManager.lua
    Added event manager

---
+++ eventManager.lua
@@ -0,0 +1,34 @@
+local PATH = (...):match("^(.-)[^%.]+$")
+
+local mintmousse = require(PATH .. "conf")
+
+local eventLogger = mintmousse._logger:extend("Event")
+
+local eventManager = {
+  eventCallbacks = { },
+}
+
+eventManager.addCallback = function(callbackID, callbackFunction)
+  eventManager.eventCallbacks[callbackID] = callbackFunction
+end
+
+eventManager.removeCallback = function(callbackID)
+  eventManager.eventCallbacks[callbackID] = nil
+end
+
+eventManager.jsEvent = function(componentID, callbackID)
+  if type(componentID) ~= "string" or type(callbackID) ~= "string" then
+    eventLogger:warning(mintmousse.EVENT_ENUM_JS_EVENT, ": expected two string arguments, instead received:", type(componentID), type(callbackID))
+    return
+  end
+
+  local callbackFunction = eventManager.eventCallbacks[callbackID]
+  if not callbackFunction then
+    return
+  end
+
+  local component = mintmousse.get(componentID)
+  callbackFunction(component)
+end
+
+return eventManager
