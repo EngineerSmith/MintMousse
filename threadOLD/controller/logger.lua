5c6703d95754c2ccc7a82721be479a8c165a62ce
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2026-03-20 15:55:09 +0000

    mega refactor

--- threadOLD/controller/logger.lua
+++
@@ -1,8 +0,0 @@
-local PATH = (...):match("^(.*)state$")
-local ROOT = PATH:match("^(.-)thread%.controller%.$")
-
-local mintmousse = require(ROOT .. "conf")
-
-local loggerController = mintmousse._logger:extend("Controller")
-
-return loggerController
